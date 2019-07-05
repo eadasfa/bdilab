2393c3982b0e075b4e1726fbb6c95153da77d934
ykkxdu <ykk_xdu@163.com>
2019-07-05 15:21:55 +0800

    初始化仓库

---
+++ src/test/java/com/bdi/lab/BdilabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bdi.lab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BdilabApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
