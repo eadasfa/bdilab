15e00bee84d56092d4139a75afe95d26f40626ec
dq <1319468035@qq.com>
2019-07-16 16:50:03 +0800

    查询Pod状态

--- src/main/java/com/bdi/lab/test/TestWithFabric8.java
+++ src/main/java/com/bdi/lab/test/TestWithFabric8.java
@@ -1,23 +1,22 @@
 package com.bdi.lab.test;
 
-import io.fabric8.kubernetes.api.model.Config;
-import io.fabric8.kubernetes.api.model.Namespace;
-import io.fabric8.kubernetes.api.model.ReplicationController;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import org.apache.commons.collections.bag.SynchronizedSortedBag;
 
 import javax.naming.Name;
 import javax.naming.ldap.PagedResultsControl;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestWithFabric8 {
     private static final String IP = "192.168.0.153";
     private static final String PORT = "8080";
     private static KubernetesClient _kube = new DefaultKubernetesClient("http://"+IP+":"+PORT);
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args)  {
 
     }
     public static void changeReplicas(String namespace, String RcName, int num ){
@@ -29,7 +28,6 @@ public class TestWithFabric8 {
                      .withReplicas(num)
                  .endSpec()
              .done() ;
-
     }
     //通过RC实现停止服务功能
     public static void stopService(String namespace, String RcName ){
@@ -37,14 +35,24 @@ public class TestWithFabric8 {
 
     }
     //通过RC实现启动服务功能
-    public static boolean startService(String namespace, String RcName ,Integer num){
+    public static boolean startService(String namespace, String RcName,Integer num){
         if(num<=0){
             System.out.println("Error: num is less than 0！");
             return false;
         }
         changeReplicas(namespace ,RcName ,num);
         return true;
-
     }
-
+    public static boolean startService(String namespace, String RcName){
+        return startService(namespace,RcName,1);
+    }
+    public static Map<String,String> queryPodPhase(){
+        Map<String, String> map = new HashMap<>();
+        List<Pod> pl = _kube.pods().inNamespace("k8s-test").withLabel("app","mysql").list()
+                .getItems();
+        for (Pod po: pl){
+            map.put(po.getMetadata().getName(),po.getStatus().getPhase());
+        }
+        return map;
+    }
 }
