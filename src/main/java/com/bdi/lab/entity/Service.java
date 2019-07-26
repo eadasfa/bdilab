da98ee91d7b6b822a784f95aa27ddb01e6e06918
dq <1319468035@qq.com>
2019-07-26 10:53:31 +0800

    将Controller返会值修改为ResponseEntity，重新修改了Service实例

--- src/main/java/com/bdi/lab/entity/Service.java
+++ src/main/java/com/bdi/lab/entity/Service.java
@@ -5,89 +5,25 @@ import io.fabric8.kubernetes.api.model.ServiceSpec;
 import io.fabric8.kubernetes.api.model.ServiceStatus;
 import java.util.Map;
 
-public class Service{
+public class Service extends io.fabric8.kubernetes.api.model.Service{
     private String state;
-    private String apiVersion;
-    private String kind;
-    private ObjectMeta metadata;
-    private ServiceSpec spec;
-    private ServiceStatus status;
-    private Map<String, Object> additionalProperties ;
 
     public Service(io.fabric8.kubernetes.api.model.Service service){
         setApiVersion(service.getApiVersion());
         setKind(service.getKind());
         setMetadata(service.getMetadata());
-        setAdditionalProperties(service.getAdditionalProperties());
         setStatus(service.getStatus());
         setSpec(service.getSpec());
+        service.getAdditionalProperties().forEach((k,v)->{
+            setAdditionalProperty(k,v);
+        });
     }
+
     public String getState() {
         return state;
     }
 
-    public void setState(String phase) {
-        this.state = phase;
-    }
-
-    public String getApiVersion() {
-        return apiVersion;
-    }
-
-    public void setApiVersion(String apiVersion) {
-        this.apiVersion = apiVersion;
-    }
-
-    public String getKind() {
-        return kind;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
-    }
-
-    public ObjectMeta getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(ObjectMeta metadata) {
-        this.metadata = metadata;
-    }
-
-    public ServiceSpec getSpec() {
-        return spec;
-    }
-
-    public void setSpec(ServiceSpec spec) {
-        this.spec = spec;
-    }
-
-    public ServiceStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ServiceStatus status) {
-        this.status = status;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return additionalProperties;
-    }
-
-    public void setAdditionalProperties(Map<String, Object> additionalProperties) {
-        this.additionalProperties = additionalProperties;
-    }
-
-    @Override
-    public String toString() {
-        return "Service(" +
-                "state=" + state  +
-                ", apiVersion=" + apiVersion  +
-                ", kind=" + kind  +
-                ", metadata=" + metadata +
-                ", spec=" + spec +
-                ", status=" + status +
-                ", additionalProperties=" + additionalProperties +
-                ')';
+    public void setState(String state) {
+        this.state = state;
     }
 }
