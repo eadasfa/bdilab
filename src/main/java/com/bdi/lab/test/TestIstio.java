c7594195a9ebb6f9c36e489df7dea8ccb1a0fb8a
dq <1319468035@qq.com>
2019-07-30 09:46:10 +0800

    添加Istio client

---
+++ src/main/java/com/bdi/lab/test/TestIstio.java
@@ -0,0 +1,13 @@
+package com.bdi.lab.test;
+
+import me.snowdrop.istio.client.DefaultIstioClient;
+import me.snowdrop.istio.client.IstioClient;
+
+import static com.bdi.lab.utils.Common.IP;
+import static com.bdi.lab.utils.Common.PORT;
+
+public class TestIstio {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/bdi/lab/utils/Common.java
+++ src/main/java/com/bdi/lab/utils/Common.java
@@ -1,13 +1,21 @@
 package com.bdi.lab.utils;
 
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import me.snowdrop.istio.client.DefaultIstioClient;
+import me.snowdrop.istio.client.IstioClient;
 
 public class Common {
     public static final String IP = "192.168.0.153";
     public static final int PORT = 8080;
     public static final String USERNAME = "root";
-    public static final String PASSWORD="111111";
+    public static final String PASSWORD="123456";
     public static final String NAMESPACE="k8s-test";
-    public static KubernetesClient _kube = new DefaultKubernetesClient("http://"+IP+":"+PORT);
+
+    public static Config config = new ConfigBuilder().withMasterUrl("http://"+IP+":"+PORT).build();
+    public static KubernetesClient _kube = new DefaultKubernetesClient(config);//使用默认的就足够了
+    public static IstioClient _istio = new DefaultIstioClient(config);
+//    public static KubernetesClient _kube = new DefaultKubernetesClient("http://"+IP+":"+PORT);
 }
